ba80a8c9cfa727d8591cae695579309810896a87
Abir Hasan <hackitabirhasan@gmail.com>
2020-11-11 12:36:19 +0600

    Update config.py

--- config.py
+++ config.py
@@ -1,13 +1,13 @@
 from tobrot.sample_config import Config
 
 class Config(Config):
-  TG_BOT_TOKEN = "1292807538:AAGa14irVFqIu_B3S5SV03I3qOahtt7p5Bg"
+  TG_BOT_TOKEN = "1400992374:AAHEvgKn7FpZrr6rlAJbV4CYl2aKo42LHaU"
   APP_ID = 1878625
   API_HASH = "3ab3e48ba126de2eb96ffff39b18fa09"
   AUTH_CHANNEL = [-1001256038785, -1001408445549, -1001398883981, -1001275361660, 715779594, 1161316820, 1291911714]
-  INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/0:/Trash"
-  YTDL_COMMAND = "ytdl@URL_Uploader_Max_Bot"
-  LEECH_COMMAND = "leech@URL_Uploader_Max_Bot"
+  INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/3:/Trash"
+  YTDL_COMMAND = "ytdl"
+  LEECH_COMMAND = "leech"
   CLEAR_THUMBNAIL = "clearthumbnail"
   SAVE_THUMBNAIL = "savethumbnail"
   OWNER_ID = 715779594
@@ -17,6 +17,6 @@ root_folder_id = 0AMrENdA1NMleUk9PVA
 token = {"access_token":"ya29.a0AfH6SMBYe5epW_wx2ThDN0fdjhcTQVPlK8qJIGceCSvRl85ZHF-jZ7a-ZlTIK4dhHD1wf9mZT12B7KlNmqghp7vpMJ1m_ezo5lTzsTqCH0NRRzq3hub9p1GOkmdkvyyeE6I9tLmHJeBQAgXO6w4jwXLsYixp5STFP-I","token_type":"Bearer","refresh_token":"1//0grYuUY9XmmF4CgYIARAAGBASNwF-L9IrdxHmM-gUJlMuI_kkR3B1bmkctuEzvOPN1w2zIdJgLfp8IFyF_ej9cqxWzk32pDTNmDI","expiry":"2020-09-05T16:05:17.0259487+06:00"}
 team_drive = 0AMrENdA1NMleUk9PVA"""
   DESTINATION_FOLDER = "Trash"
-  GLEECH_COMMAND = "gleech@URL_Uploader_Max_Bot"
-  TELEGRAM_LEECH_COMMAND_G = "tleech@URL_Uploader_Max_Bot"
-  PYTDL_COMMAND_G = "pytdl@URL_Uploader_Max_Bot"
+  GLEECH_COMMAND = "gleech"
+  TELEGRAM_LEECH_COMMAND_G = "tleech"
+  PYTDL_COMMAND_G = "pytdl"
