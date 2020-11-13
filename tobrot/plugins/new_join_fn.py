6ed1fdf187e733d5ed8b41374ef8aaf7734b5067
RJ HANIF AHMED <66148227+MaxxRider@users.noreply.github.com>
2020-11-13 07:28:44 +0530

    Update new_join_fn.py

--- tobrot/plugins/new_join_fn.py
+++ tobrot/plugins/new_join_fn.py
@@ -1,4 +1,3 @@
-
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # (c) Shrimadhav U K
@@ -24,35 +23,14 @@ async def new_join_f(client, message):
     chat_type = message.chat.type
     if chat_type != "private":
         await message.reply_text(f"Current CHAT ID: <code>{message.chat.id}</code>")
-        # leave chat
         await client.leave_chat(
             chat_id=message.chat.id,
             delete=True
         )
-    # delete all other messages, except for AUTH_CHANNEL
     await message.delete(revoke=True)
  
  
 async def help_message_f(client, message):
-    # await message.reply_text("no one gonna help you 🤣🤣🤣🤣", quote=True)
-    #channel_id = str(AUTH_CHANNEL)[4:]
-    #message_id = 99
-    # display the /help
     
-    await message.reply_text(""" """)
+    await message.reply_text("""Hi Bro""")
  
- 
-async def rename_message_f(client, message):
-    inline_keyboard = []
-    inline_keyboard.append([
-        pyrogram.InlineKeyboardButton(
-            text="read this?",
-            url="https://t.me/keralagram/698909"
-        )
-    ])
-    reply_markup = pyrogram.InlineKeyboardMarkup(inline_keyboard)
-    await message.reply_text(
-        "please use @renamebot",
-        quote=True,
-        reply_markup=reply_markup
-    )
