4c74886428b2d8eb02eb97036dad64bcf1f0d4b9
Abir Hasan <hackitabirhasan@gmail.com>
2020-11-18 01:39:18 +0600

    Update config.py

--- tobrot/config.py
+++ tobrot/config.py
@@ -1,22 +1,23 @@
 from tobrot.sample_config import Config
 
 class Config(Config):
-  TG_BOT_TOKEN = "1461078281:AAGQrSqbHBgyVVR5BtnvE6wf2pZEkF-lslw"
+  TG_BOT_TOKEN = "1287349352:AAE5kpUUv5La3JDxfl2SFpl3SHu0dcqrOzw"
   APP_ID = 1878625
   API_HASH = "3ab3e48ba126de2eb96ffff39b18fa09"
-  AUTH_CHANNEL = [-1001256038785, -1001408445549, -1001398883981, -1001275361660, 715779594, 1161316820, 1291911714]
-  INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/3:/Trash"
-  YTDL_COMMAND = "ytdl"
-  LEECH_COMMAND = "leech"
-  CLEAR_THUMBNAIL = "clearthumbnail"
-  SAVE_THUMBNAIL = "savethumbnail"
+  AUTH_CHANNEL = [-1001256038785, -1001408445549, -1001398883981, -1001275361660, -1001273851623]
+  INDEX_LINK = "https://tgindex.hackitabirhasan.workers.dev/3:/Uploads"
+  YTDL_COMMAND = "ytdl@abir_leeching_bot"
+  LEECH_COMMAND = "leech@abir_leeching_bot"
+  CLEAR_THUMBNAIL = "clearthumbnail@abir_leeching_bot"
+  SAVE_THUMBNAIL = "savethumbnail@abir_leeching_bot"
   OWNER_ID = 715779594
   RCLONE_CONFIG = """type = drive
 scope = drive
-root_folder_id = 0AE7pdvzvClR_Uk9PVA
-token = {"access_token":"ya29.A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}
-team_drive = 0AE7pdvzvClR_Uk9PVA"""
-  DESTINATION_FOLDER = "Trash"
-  GLEECH_COMMAND = "gleech"
-  TELEGRAM_LEECH_COMMAND_G = "tleech"
-  PYTDL_COMMAND_G = "pytdl"
+root_folder_id = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4
+token = {"access_token":"ya29A0AfH6SMCaLwuWkNALk5I4IWBZP6y5Y4lZRkgepXoaf-LvSu2jmNDyLzezwJk-HlgcP1OSiRKqAa9qKsCBMv9O2IMd1ebAwhYdVoNP7jIXMRBDKzD10qfrzq7oGda_Z-vP3Qvl6PQcOYC3x5ecEkU3vMIyqEC2xgANsEnowpSENeU","token_type":"Bearer","refresh_token":"1//0gHp6knwySTm9CgYIARAAGBASNwF-L9IrRPO94mU1d9kBkUu1BbYrkA_EMhGXcCiHXGULiAZDg5BmaSxhrtALRfBmVpiotPEtK6Q","expiry":"2020-11-11T13:38:54.4701885+06:00"}
+team_drive = 1rWomzK1yVjkqIbC9p9EBBKJgcf3imGr4"""
+  DESTINATION_FOLDER = ""
+  GLEECH_COMMAND = "gleech@abir_leeching_bot"
+  TELEGRAM_LEECH_COMMAND_G = "tleech@abir_leeching_bot"
+  PYTDL_COMMAND_G = "pytdl@abir_leeching_bot"
+  MAX_TG_SPLIT_FILE_SIZE = 2097152000
